9eb22ffc4f5188158701e5fa2150a80f0a7ebb55
Ben Franske <ben.mm@franske.com>
2023-07-11 12:10:59 -0500

    initial commit

---
+++ depot-monitor.py
@@ -0,0 +1,33 @@
+#!/usr/bun/python3
+import PyNUT
+
+def setup():
+    global ups
+    ups = PyNUT.PyNUTClient(host='localhost', timeout=300)
+    global upsVarsLast
+    global toMonitor
+    upsVarsLast = {}
+    toMonitor = ['input.voltage', 'ups.status']
+
+def loop():
+    global ups
+    global upsVarsLast
+    global toMonitor
+    allUpsVars = {}
+    upsVars = {}
+    allUpsVars= ups.GetUPSVars(ups='chsl-depot-rack')
+    allUpsVars = {y.decode('ascii'): upsVars.get(y).decode('ascii') for y in upsVars.keys()}
+    for variable in toMonitor:
+        upsVars[variable] = allUpsVars[variable]
+    if upsVars !=upsVarsLast:
+        print(upsVars)
+    return
+
+def main():
+    setup()
+    while True:
+        loop()
+    return
+
+if __name__ == "__main__":
+    main()
