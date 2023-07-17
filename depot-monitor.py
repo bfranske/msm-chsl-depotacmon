0f428e9d139f17f2e8aed32ba35132e50dfc893b
Ben Franske <ben.mm@franske.com>
2023-07-16 21:16:58 -0500

    add UPS reconnection if stale data

--- depot-monitor.py
+++ depot-monitor.py
@@ -18,18 +18,34 @@ def loop():
     global toMonitor
     allUpsVars = {}
     upsVars = {}
-    allUpsVars= ups.GetUPSVars(ups='chsl-depot-rack')
-    allUpsVars = {y.decode('ascii'): allUpsVars.get(y).decode('ascii') for y in allUpsVars.keys()}
-    now = datetime.datetime.now()
-    for variable in toMonitor:
-        upsVars[variable] = allUpsVars[variable]
-    if upsVars !=upsVarsLast:
-        dataPoint = {'date':now.strftime('%Y-%m-%d'), 'time':now.strftime('%H:%M:%S')}
-        dataPoint.update(upsVars)
-        with open('/home/pi/depotacdata.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(dataPoint.values())
-    upsVarsLast = upsVars
+    try:
+        allUpsVars= ups.GetUPSVars(ups='chsl-depot-rack')
+        allUpsVars = {y.decode('ascii'): allUpsVars.get(y).decode('ascii') for y in allUpsVars.keys()}
+        now = datetime.datetime.now()
+        for variable in toMonitor:
+            upsVars[variable] = allUpsVars[variable]
+        if upsVars !=upsVarsLast:
+            dataPoint = {'date':now.strftime('%Y-%m-%d'), 'time':now.strftime('%H:%M:%S')}
+            dataPoint.update(upsVars)
+            with open('/home/pi/depotacdata.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(dataPoint.values())
+        upsVarsLast = upsVars
+    except PyNUT.PyNUTError:
+        #Probably stale data, reconnect to the UPS
+        ups = PyNUT.PyNUTClient(host='localhost', timeout=300)
+        allUpsVars= ups.GetUPSVars(ups='chsl-depot-rack')
+        allUpsVars = {y.decode('ascii'): allUpsVars.get(y).decode('ascii') for y in allUpsVars.keys()}
+        now = datetime.datetime.now()
+        for variable in toMonitor:
+            upsVars[variable] = allUpsVars[variable]
+        if upsVars !=upsVarsLast:
+            dataPoint = {'date':now.strftime('%Y-%m-%d'), 'time':now.strftime('%H:%M:%S')}
+            dataPoint.update(upsVars)
+            with open('/home/pi/depotacdata.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(dataPoint.values())
+        upsVarsLast = upsVars
     return
 
 def main():
